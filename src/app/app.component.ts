d3dd09d1047a92b1d9b584d0ba083027b195c894
Mehul247 <mehul.bhunsadiya@bacancytechnology.com>
2020-06-02 18:00:19 +0530

    drag and drop with popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Drag-image';
+  display: boolean = false;
+
+  list: string[];
+  constructor(){}
+
+  ngOnInit(){
+    this.list = ["Red","Blue","Green","Pink"]
+  }
+  drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    this.display = true;
+  }
+
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  drag(ev) {
+    console.log( ev.target.id)
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  onSubmit(f: any): void {
+    this.display = false;
+  }
 }
